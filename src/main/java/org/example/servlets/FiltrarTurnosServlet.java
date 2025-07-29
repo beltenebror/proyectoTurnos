244dbfa61d95d2d8fc9698663c77f3f80a1be64d
JuanMarti <previlsa@previlsa.com>
2025-07-29 19:52:34 +0200

    Estructura base Filtrar Turnos

---
+++ src/main/java/org/example/servlets/FiltrarTurnosServlet.java
@@ -0,0 +1,10 @@
+package org.example.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class FiltrarTurnosServlet extends HttpServlet {
+
+
+}
