3d099045de0e159df4bcffe42fd6c560ff54989d
JuanMarti <previlsa@previlsa.com>
2025-07-31 14:43:31 +0200

    AgregarTurnoServlet a JPA

--- src/main/java/org/example/utils/TurnoJPA.java
+++ src/main/java/org/example/utils/TurnoJPA.java
@@ -1,8 +1,12 @@
 package org.example.utils;
 
 import jakarta.persistence.*;
+import org.example.entities.Ciudadano;
 import org.example.entities.EstadoTurnos;
 import org.example.entities.Turno;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TurnoJPA {
 //    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("miUnidad");
@@ -35,4 +39,28 @@ public class TurnoJPA {
         }
         return turno;
     }
+
+    public static Turno agregarTurno(LocalDate fecha, LocalTime hora, EstadoTurnos estado, String idCiudadano, String descripcion){
+        EntityManager em = ConfigJPA.getEntityManager();
+        Turno turno = new Turno();
+
+        try {
+            em.getTransaction().begin();
+
+            Ciudadano ciudadano = em.createQuery("SELECT c FROM Ciudadano c WHERE c.id="+idCiudadano, Ciudadano.class).getSingleResult();
+
+//            Turno turno = new Turno();
+            turno.setFecha(fecha);
+            turno.setHora(hora);
+            turno.setEstado(estado);
+            turno.setCiudadano(ciudadano);
+            turno.setCodigo(turno.generarCodigo());
+            turno.setDescripcion(descripcion);
+            em.persist(turno);
+            em.getTransaction().commit();
+    } finally {
+            em.close();
+        }
+        return turno;
+        }
 }
